69d0bd8a7cf328585c1272b8231a438971a08d6a
Saku Ytti <saku@ytti.fi>
2018-12-19 12:09:34 +0200

    rubycop fixes

--- lib/oxidized/model/grandstream.rb
+++ lib/oxidized/model/grandstream.rb
@@ -1,6 +1,6 @@
 class GrandStream < Oxidized::Model
-  cmd "/cgi-bin/dologin?password=%{password}" do |cfg|
-    sid = JSON.load(cfg)["body"]["sid"]
+  cmd "/cgi-bin/dologin?password=%<password>s" do |cfg|
+    sid = JSON.parse(cfg)["body"]["sid"]
     cmd "/cgi-bin/download_cfg?sid=#{sid}"
   end
 
